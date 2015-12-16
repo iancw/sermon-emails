a750db1ef7b18b6cb66dffaf15c1ff2f052b09b1
Ian Will <icwill@gmail.com>
2015-12-15 22:12:23 -0500

    Move code into properties

--- parse.rb
+++ parse.rb
@@ -1,8 +1,30 @@
 require 'yaml'
-require 'net/http'
 require 'date'
+require 'net/http'
+require 'net/smtp'
 
-KEY='IP'
+class Sermon
+  attr_accessor :date, :passage, :title, :text
+  def initialize hash, text
+    @date = hash['date']
+    @passage = hash['passage']
+    @title = hash['title']
+    @text = text
+  end
+
+  def subject
+    "Sermon for #{date}: #{passage} - #{title}"
+  end
+end
+
+class Contact
+  attr_accessor :name, :email
+
+  def initialize name, email
+    @name = name
+    @email = email
+  end
+end
 
 def urlify passage
   passage.gsub(/ /, '+')
@@ -19,8 +41,45 @@ def find_next_sermon upcoming
   raise 'Unable to find any upcoming sermons'
 end
 
-upcoming = YAML.load_file('upcoming.yml')
-sermon = find_next_sermon upcoming
-text = Net::HTTP.get(URI.parse("http://www.esvapi.org/v2/rest/passageQuery?key=#{KEY}&passage=#{urlify(sermon['passage'])}&include-headings=false"))
+def upcoming_sermon esv_key
+  upcoming = YAML.load_file('upcoming.yml')
+  sermon_hash = find_next_sermon upcoming
+  text = Net::HTTP.get(URI.parse("http://www.esvapi.org/v2/rest/passageQuery?key=#{esv_key}&passage=#{urlify(sermon_hash['passage'])}&include-headings=false"))
+  Sermon.new sermon_hash, text
+end
 
-puts text
+def send_message args
+  sender = args[:sender] || raise ArgumentError.new('Must supply sender')
+  recipient = args[:recipient] || raise ArgumentError.new('Must supply recipient')
+  sermon = args[:sermon] || raise ArgumentError.new('Must supply sermon')
+  message = <<MESSAGE_END
+From: #{sender.name} <#{sender.email}>
+To: #{recipient.name} <#{recipient.email}>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: #{sermon.subject}
+
+#{sermon.text}
+
+MESSAGE_END
+
+  Net::SMTP.start('localhost') do |smtp|
+      smtp.send_message message, sender.email, recipient.email
+  end
+end
+
+def load_recipients yml_file
+  YAML.load_file(yml_file).map{ |r| Contact.new(r['name'], r['email']) }
+end
+
+def run
+  props = YAML.load_file('config.yml')
+  sender = Contact.new(props['from_name'], props['from_email'])
+  esv_key = props['esv_key']
+
+  sermon = upcoming_sermon esv_key
+  recipients = load_recipients 'recipients.yml'
+  recipients.each do | recipient|
+    send_message(recipient: recipient, sermon: sermon, sender: sender)
+  end
+end
