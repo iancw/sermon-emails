5b07f28b35886e087239b75cbd4a899670b25292
Ian Will <icwill@gmail.com>
2017-01-04 08:50:05 -0500

    Pass AWS credentials to S3 instance

--- js/fetch-s3.js
+++ js/fetch-s3.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const awsConfig = require('./_ses_private');
 
 module.exports = fetchS3;
 
 function* fetchS3(opts) {
-  const s3 = new AWS.S3();
+  const s3 = new AWS.S3({
+    accessKeyId: awsConfig.AccessKeyId,
+    secretAccessKey: awsConfig.SecretAccessKey
+  });
   return yield function(cb) {
     s3.getObject({
       Bucket: opts.bucket,
